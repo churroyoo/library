251eda3f46a84d04402bcde8105b213f679fe65d
Churro <27313833+churrou@users.noreply.github.com>
2024-01-08 14:31:05 -0800

    Create ability to add books
    We can create books using the prompt, however we
    need to work on the updateLibrary function
    that displays the library array. It's not duplicating
    books everytime we update.

--- script.js
+++ script.js
@@ -19,3 +19,30 @@ function addBookToLibrary(){
     let book = new Book(title, author, pages, read)
     library.push(book)
 }
+
+const books = document.querySelector('.books');
+const addBookButton = document.querySelector('.addBook')
+addBookButton.addEventListener('click', () =>{
+  addBookToLibrary()
+  updateLibrary()
+})
+
+function updateLibrary(){
+  for (let i = 0; i < library.length; i++){
+    const card = document.createElement('div')
+    const bookTitle = document.createElement('h3')
+    const bookAuthor = document.createElement('h4')
+    const bookPages = document.createElement('h4')
+
+    bookTitle.textContent = library[i].title
+    bookAuthor.textContent = library[i].author
+    bookPages.textContent = library[i].pages
+
+    card.appendChild(bookTitle)
+    card.appendChild(bookAuthor)
+    card.appendChild(bookPages)
+
+    books.appendChild(card)
+  }
+}
+
